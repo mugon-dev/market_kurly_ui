cc91a3771596207af906dc38cc51a58557883d5b
mugon-dev <jmg41490400@gmail.com>
2021-09-04 17:16:39 +0900

    feat: add my_kurly screen appbar, header

--- lib/screens/my_kurly/my_kurly_screen.dart
+++ lib/screens/my_kurly/my_kurly_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:market_kurly_ui/screens/components/custom_actions.dart';
+import 'package:market_kurly_ui/screens/components/default_button.dart';
 
 class MyKurlyScreen extends StatelessWidget {
   const MyKurlyScreen({Key? key}) : super(key: key);
@@ -6,8 +8,47 @@ class MyKurlyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        // 뒤로 가기 버튼 자동 생성할 것인지
+        automaticallyImplyLeading: false,
+        title: Text('마이컬리'),
+        actions: [CustomActions()],
+      ),
       body: Center(
-        child: Text('MyKurlyScreen'),
+        child: SizedBox(
+          height: 250,
+          width: double.infinity,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Spacer(),
+                SizedBox(
+                  width: 250,
+                  child: Text(
+                    '회원 가입하고\n 다양한 혜택을 받아보세요!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  '가입 혜택 보기 >',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: DefaultButton(
+                    text: '로그인/회원가입',
+                    press: () {},
+                  ),
+                ),
+                Spacer(),
+              ]),
+        ),
       ),
     );
   }
