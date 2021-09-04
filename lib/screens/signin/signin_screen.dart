fdf657baa4dce80e4323ef6b4d88545265d3a091
mugon-dev <jmg41490400@gmail.com>
2021-09-04 18:21:33 +0900

    feat: add signin screen appbar

--- lib/screens/signin/signin_screen.dart
+++ lib/screens/signin/signin_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:market_kurly_ui/theme.dart';
 
 class SignInScreen extends StatelessWidget {
   static String routeName = '/sign_in';
@@ -8,9 +9,25 @@ class SignInScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('로그인'),
+        title: Text(
+          '로그인',
+          style: textTheme().subtitle1?.copyWith(color: Colors.black),
+        ),
         // 뒤로 가기 아이콘, 기능 자동으로 생성
-        automaticallyImplyLeading: true,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+        bottom: PreferredSize(
+          child: Divider(),
+          preferredSize: Size.fromHeight(1.0),
+        ),
       ),
       body: Container(),
     );
