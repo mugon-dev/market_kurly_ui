44aa37c83dc6618312d52d22dc0b5f2605524765
mugon-dev <jmg41490400@gmail.com>
2021-09-04 15:47:40 +0900

    feat: add indexed widget

--- lib/screens/main_screens.dart
+++ lib/screens/main_screens.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:market_kurly_ui/screens/category/category_screen.dart';
+import 'package:market_kurly_ui/screens/home/home_screen.dart';
+import 'package:market_kurly_ui/screens/my_kurly/my_kurly_screen.dart';
+import 'package:market_kurly_ui/screens/recommend/recommend_screen.dart';
+import 'package:market_kurly_ui/screens/search/search_screen.dart';
 
 class MainScreen extends StatefulWidget {
   static String routeName = 'main_screens';
@@ -9,12 +14,19 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('mainScreen'),
-      ),
-    );
+        body: IndexedStack(
+      index: _selectedIndex,
+      children: [
+        HomeScreen(),
+        RecommendScreen(),
+        CategoryScreen(),
+        SearchScreen(),
+        MyKurlyScreen(),
+      ],
+    ));
   }
 }
