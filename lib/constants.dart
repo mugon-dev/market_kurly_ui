560432b7a30e9859d9eb6d27f20ed3776655319a
mugon-dev <jmg41490400@gmail.com>
2021-09-04 15:22:50 +0900

    style: add main style

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,7 @@
 // 대표 색상 및  상수 관리 파일
+
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF5f0088);
+const kSecondaryColor = Color(0xff979797);
+const kTextColor = Color(0xff757575);
