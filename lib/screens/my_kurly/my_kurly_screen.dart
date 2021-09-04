58a974e7d4ff07a003e253b37acbb15f07a5c930
mugon-dev <jmg41490400@gmail.com>
2021-09-04 17:51:19 +0900

    feat: add my_kurly component in screen

--- lib/screens/my_kurly/my_kurly_screen.dart
+++ lib/screens/my_kurly/my_kurly_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:market_kurly_ui/screens/components/custom_actions.dart';
-import 'package:market_kurly_ui/screens/components/default_button.dart';
-import 'package:market_kurly_ui/screens/my_kurly/components/my_kurly_header.dart';
+import 'package:market_kurly_ui/screens/my_kurly/components/my_kurly_body.dart';
 
 class MyKurlyScreen extends StatelessWidget {
   const MyKurlyScreen({Key? key}) : super(key: key);
@@ -9,14 +8,15 @@ class MyKurlyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          // 뒤로 가기 버튼 자동 생성할 것인지
-          automaticallyImplyLeading: false,
-          title: Text('마이컬리'),
-          actions: [CustomActions()],
-        ),
-        body: Container(
-          child: MyKurlyHeader(),
-        ));
+      appBar: AppBar(
+        // 뒤로 가기 버튼 자동 생성할 것인지
+        automaticallyImplyLeading: false,
+        title: Text('마이컬리'),
+        actions: [CustomActions()],
+      ),
+      body: SingleChildScrollView(
+        child: MyKurlyBody(),
+      ),
+    );
   }
 }
