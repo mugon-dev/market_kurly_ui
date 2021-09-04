1bf484fafb4216213ae52b103cd1d64a970e8260
mugon-dev <jmg41490400@gmail.com>
2021-09-04 15:43:12 +0900

    feat: add sample code

--- lib/screens/main_screens.dart
+++ lib/screens/main_screens.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   static String routeName = 'main_screens';
   const MainScreen({Key? key}) : super(key: key);
 
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
