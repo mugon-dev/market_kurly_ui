6d8cf43c4e34b32dfaffeb817522ac3d4772e331
mugon-dev <jmg41490400@gmail.com>
2021-09-04 15:24:42 +0900

    feat: add route main screen, splash screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,1 +1,9 @@
 // 라우터 관리 파일( 화면 이동 경로 정의 )
+import 'package:flutter/material.dart';
+import 'package:market_kurly_ui/screens/main_screens.dart';
+import 'package:market_kurly_ui/screens/splash/splash_screen.dart';
+
+final Map<String, WidgetBuilder> route = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+  MainScreen.routeName: (context) => MainScreen(),
+};
