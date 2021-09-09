40e111f80cdd37859cde93219b95e237031baac5
mugon-dev <jmg41490400@gmail.com>
2021-09-09 16:14:07 +0900

    feat: add category screen structure

--- lib/screens/category/category_screen.dart
+++ lib/screens/category/category_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:market_kurly_ui/constants.dart';
+import 'package:market_kurly_ui/screens/components/custom_actions.dart';
+import 'package:market_kurly_ui/screens/components/text_menu_card.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({Key? key}) : super(key: key);
@@ -6,8 +9,31 @@ class CategoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('CategoryScreen'),
+      appBar: AppBar(
+        title: Text("카테고리"),
+        actions: [CustomActions()],
+        automaticallyImplyLeading: false,
+      ),
+      body: CustomScrollView(
+        slivers: [
+          // sliver 안에 패딩을 줄때 사용
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            // customScrollView 안에 단일 위젯을 생성할 때 사용
+            sliver: SliverToBoxAdapter(
+              child: SizedBox(
+                height: 60,
+                child: TextMenuCard(
+                  title: "자주 사는 상품",
+                  icon: "assets/icons/right-arrow.svg",
+                  textColor: kPrimaryColor,
+                  iconColor: kPrimaryColor,
+                  press: () {},
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
