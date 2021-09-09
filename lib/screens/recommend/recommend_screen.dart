5c0ba1d801a98e707d2b90c1bd611fd88e293fa2
mugon-dev <jmg41490400@gmail.com>
2021-09-09 18:53:29 +0900

    feat: add recommend screen structure

--- lib/screens/recommend/recommend_screen.dart
+++ lib/screens/recommend/recommend_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:market_kurly_ui/models/product.dart';
+import 'package:market_kurly_ui/screens/recommend/components/card_product_item.dart';
+import 'package:market_kurly_ui/theme.dart';
 
 class RecommendScreen extends StatelessWidget {
   const RecommendScreen({Key? key}) : super(key: key);
@@ -6,8 +9,56 @@ class RecommendScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('RecommendScreen'),
+      appBar: AppBar(
+        title: Text("추천"),
+        automaticallyImplyLeading: false,
+      ),
+      body: ListView(
+        children: [
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Text(
+              "베이커리 인기 급상승 랭킹",
+              style: textTheme().headline1,
+            ),
+          ),
+          const SizedBox(height: 15),
+          Container(
+            color: Colors.orange[100],
+            height: 270,
+            child: Center(child: Text('수평 스크롤 영역')),
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Text(
+              "후기가 좋은 상품",
+              style: textTheme().headline1,
+            ),
+          ),
+          const SizedBox(height: 15),
+          Container(
+            color: Colors.red[100],
+            height: 270,
+            child: Center(child: Text('수평 스크롤 영역2')),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            height: 360,
+            child: ListView.builder(
+              padding: const EdgeInsets.only(bottom: 40),
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => SizedBox(
+                width: 320,
+                child: CardProductItem(
+                  item: productList[index],
+                ),
+              ),
+              itemCount: productList.length,
+            ),
+          ),
+        ],
       ),
     );
   }
