ec574401527dd7e232c2d6642702ea7be3ca83b5
mugon-dev <jmg41490400@gmail.com>
2021-09-09 15:35:58 +0900

    feat: add details page route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,8 @@
 // 라우터 관리 파일( 화면 이동 경로 정의 )
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:market_kurly_ui/screens/details/details_screen.dart';
 import 'package:market_kurly_ui/screens/main_screens.dart';
 import 'package:market_kurly_ui/screens/signin/signin_screen.dart';
 import 'package:market_kurly_ui/screens/splash/splash_screen.dart';
@@ -8,4 +11,5 @@ final Map<String, WidgetBuilder> route = {
   SplashScreen.routeName: (context) => SplashScreen(),
   MainScreen.routeName: (context) => MainScreen(),
   SignInScreen.routeName: (context) => SignInScreen(),
+  DetailsScreen.routeName: (context) => DetailsScreen(),
 };
