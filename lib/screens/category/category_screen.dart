72bebaaeee1443b9a817a20f4b9643e610b16c22
mugon-dev <jmg41490400@gmail.com>
2021-09-09 17:01:35 +0900

    feat: add category screen

--- lib/screens/category/category_screen.dart
+++ lib/screens/category/category_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:market_kurly_ui/constants.dart';
+import 'package:market_kurly_ui/models/grid_category_menu.dart';
+import 'package:market_kurly_ui/models/list_category_menu.dart';
+import 'package:market_kurly_ui/screens/category/components/extends_icon_text_card.dart';
+import 'package:market_kurly_ui/screens/category/components/image_text_card.dart';
 import 'package:market_kurly_ui/screens/components/custom_actions.dart';
 import 'package:market_kurly_ui/screens/components/text_menu_card.dart';
 
@@ -30,6 +34,37 @@ class CategoryScreen extends StatelessWidget {
                   iconColor: kPrimaryColor,
                   press: () {},
                 ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Divider(height: 12, color: Colors.grey[200], thickness: 12),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              List.generate(
+                  listCategoryMenuList.length,
+                  (index) =>
+                      ExtendsIconTextCard(item: listCategoryMenuList[index])),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Divider(height: 12, color: Colors.grey[200], thickness: 12),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(20, 40, 0, 20),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200.0,
+                mainAxisSpacing: 16.0,
+                crossAxisSpacing: 10,
+                childAspectRatio: 1,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return ImageTextCard(item: gridCategoryMenuList[index]);
+                },
+                childCount: gridCategoryMenuList.length,
               ),
             ),
           )
