148d8ac988c9c086ca46191c25bd874aa61745cf
mugon-dev <jmg41490400@gmail.com>
2021-09-09 15:49:10 +0900

    feat: add detail page

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:market_kurly_ui/constants.dart';
 import 'package:market_kurly_ui/models/product.dart';
+import 'package:market_kurly_ui/models/product_details_arguments.dart';
 import 'package:market_kurly_ui/screens/components/default_button.dart';
 import 'package:market_kurly_ui/screens/details/components/body.dart';
 import 'package:market_kurly_ui/theme.dart';
@@ -11,6 +12,10 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 화면이 갱신될때 (build될 때) ModalRoute 객체를 이용해서 arguments 속성을 받을 수 있음
+    // arguments 타입이 object?이기 때문에 형변환 필요
+    final argument =
+        ModalRoute.of(context)!.settings.arguments as ProductDetailsArguments;
     return Scaffold(
       appBar: AppBar(
         iconTheme: const IconThemeData(color: Colors.black),
@@ -20,8 +25,9 @@ class DetailsScreen extends StatelessWidget {
           style: textTheme().subtitle1?.copyWith(color: Colors.black),
         ),
       ),
+      // Body components에 argument 전달
       body: Body(
-        product: productList[0],
+        product: argument.product,
       ),
       bottomNavigationBar: SafeArea(
         // column은 bottomNavigatorBar 크기에 제약을 주기위해 사용
